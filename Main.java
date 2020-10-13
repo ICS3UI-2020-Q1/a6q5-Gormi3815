01a856ee5f1f7b10e87508561903c98ad0fcf818
Gormi3815 <gormi3815@wrdsb.ca>
2020-10-13 15:42:30 +0000

    this program will ask the user for two numbers and then tell the user the ascending order of the numbers h

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
-/**
+import java.util.Scanner;
+/** THis program will prompt the user for two integers, it will then tell the user the acending order of the numbers.
  *
- * @author 
+ * @author Isaac
  */
 public class Main {
 
@@ -10,6 +11,23 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter two numbers.");
+    int[] num = new int [2];
     
+    for(int i = 0; i < num.length; i++){
+     num[i] = input.nextInt();
+     
+    }
+     if (num[0] < num[1]){
+       
+     System.out.println("The numbers in ascending order are " + num[0] + ", " + num[1]);
+     }
+     else if (num[0] > num[1]){
+       
+       System.out.println("The numbers in ascending order are " + num[1] + ", " + num[0]);
+     }
+    }
   }
-}
+  
+
